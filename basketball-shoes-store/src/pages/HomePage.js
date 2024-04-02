2c306295920d09ce5c3533874ac1ffd826d191ba
nicolasrodriguesdacruz <nrodriguesdacruz@itba.edu.ar>
2024-04-02 20:51:30 -0300

    8 commit

--- basketball-shoes-store/src/pages/HomePage.js
+++ basketball-shoes-store/src/pages/HomePage.js
@@ -4,7 +4,7 @@ import products from '../data/products';
 
 const HomePage = () => {
   return (
-    <div className="home-page" style={{ backgroundImage: 'url(/images/background.png)' }}>
+    <div className="home-page" style={{ backgroundImage: 'url(C:\Users\nicol\OneDrive\Documentos\GitHub\Progra_web\basketball-shoes-store\public\images\background.png)' }}>
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
