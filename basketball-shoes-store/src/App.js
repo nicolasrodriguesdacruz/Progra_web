5d370ed8e022ec777d4c1cc9802740f9d4f005d8
nicolasrodriguesdacruz <nrodriguesdacruz@itba.edu.ar>
2024-04-09 15:54:49 -0300

    9 commit

--- basketball-shoes-store/src/App.js
+++ basketball-shoes-store/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HomePage from './pages/HomePage';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import './App.css'; // Assuming you have an App.css for general styles
+import './styles.css'; // Assuming you have an App.css for general styles
 
 const App = () => {
   return (
