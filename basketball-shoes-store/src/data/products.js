2a2ffcef2a21b390080f4b9a105bb13a3209d268
nicolasrodriguesdacruz <nrodriguesdacruz@itba.edu.ar>
2024-04-09 17:09:12 -0300

    10 commit

--- basketball-shoes-store/src/data/products.js
+++ basketball-shoes-store/src/data/products.js
@@ -1,5 +1,8 @@
 import shoe1 from '../assets/shoe1.jpg';
 import shoe2 from '../assets/shoe2.jpg';
+import shoe3 from '../assets/shoe3.jpg';
+import shoe4 from '../assets/shoe4.jpg';
+
 
 const products = [
   {
@@ -15,6 +18,20 @@ const products = [
     price: "120",
     imageUrl: shoe2, 
     description: "Stylish and comfortable shoe with excellent ankle support."
+  },
+  {
+    id: 3,
+    name: "CURRY 11 New Curry",
+    price: "110",
+    imageUrl: shoe3, 
+    description: "The shoes you need to be a three point speacilist."
+  },
+  {
+    id: 4,
+    name: "HARDEN VOL. 4",
+    price: "130",
+    imageUrl: shoe4, 
+    description: "Durable agility meets explosive speed with James Harden's signature shoes, crafted for playmakers on the court."
   },
 ];
 
