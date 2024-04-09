0bfdeef9e2c744451788355a2de0e1eb8d4c95e3
nicolasrodriguesdacruz <nrodriguesdacruz@itba.edu.ar>
2024-04-09 16:10:27 -0300

    9 commit

--- basketball-shoes-store/src/data/products.js
+++ basketball-shoes-store/src/data/products.js
@@ -1,19 +1,22 @@
+import shoe1 from './assets/shoe1.jpg';
+import shoe2 from './assets/shoe2.jpg';
+
 const products = [
-    {
-      id: 1,
-      name: "Basketball Shoe 1",
-      price: "100",
-      imageUrl: "shoe1.jpg",
-      description: "High-quality basketball shoe for professional players."
-    },
-    {
-      id: 2,
-      name: "Basketball Shoe 2",
-      price: "120",
-      imageUrl: "shoe2.jpg",
-      description: "Stylish and comfortable shoe with excellent ankle support."
-    },
-    // Add more products as needed
-  ];
-  
-  export default products;
+  {
+    id: 1,
+    name: "Basketball Shoe 1",
+    price: "100",
+    imageUrl: shoe1, // Reference the imported image here
+    description: "High-quality basketball shoe for professional players."
+  },
+  {
+    id: 2,
+    name: "Basketball Shoe 2",
+    price: "120",
+    imageUrl: shoe2, // And here
+    description: "Stylish and comfortable shoe with excellent ankle support."
+  },
+  // Add more products as needed
+];
+
+export default products;
