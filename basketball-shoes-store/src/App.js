a37294b4a9f7e925dd105fc7288e462143dfa837
nicolasrodriguesdacruz <nrodriguesdacruz@itba.edu.ar>
2024-04-25 20:55:08 -0300

    Vamos commit

--- basketball-shoes-store/src/App.js
+++ basketball-shoes-store/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import HomePage from './pages/HomePage'; // Use HomePage
+import HomePage from './pages/HomePage'; 
 //import CartPage from './pages/CartPage';
 //import ContactPage from './pages/ContactPage';
 import Footer from './components/Footer';
-import './styles.css'; // Ensure you import your stylesheet
+import './styles.css'; 
 
 const App = () => {
   return (
